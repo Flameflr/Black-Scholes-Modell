829055980d679225852cb078e5f018aa2b2876e8
Yannick Getto <yannickgetto47@gmail.com>
2025-12-22 22:31:39 +0100

    First structur of the website

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+
+st.title("Black Scholes Modell")
+
+st.write("The Black Scholes model is a mathematical framework used to determine the fair price of European call and put options by modeling the underlying asset price as a stochastic process with constant volatility and a constant risk-free interest rate.")
+
+st.latex("S_{ t }N(d_{ 1 }) - Ke^{-rt}N(d_{ 2 })")
