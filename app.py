ac680ede6ee83aa52b268693ce6596f25d94d906
Yannick Getto <yannickgetto47@gmail.com>
2025-12-24 14:54:57 +0100

    done with the overview/explaination page. Next step: either building a second explaination page with graphs and pictures, or building the page where the people can create their own option prices.

--- app.py
+++ app.py
@@ -15,13 +15,29 @@ st.markdown("**A** = current asset price  \n**K** = strike price  \n**r** = risk
 
 col1, col2 = st.columns(2, border=True)
 
-with col1:
+with col2:
+    st.space(size="small")
     st.latex(r"d_1 = \frac{ ln(\frac{ A }{ K })+(r+\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }")
     st.markdown("the probability that the option will be exercised", text_alignment= "center")
 
-    
+    st.space(size="small")
+    st.markdown("**The formula is divided into two parameters:**", text_alignment= "center")
+    st.markdown("1.probability option ends in the money  \n2. probability how far option ends in the money")
+    st.caption("to understand the formula, you need to understand the structure from $d_2$", text_alignment= "center")
 
-with col2:
+    st.space(size="small")
+    st.markdown("<u>probability option ends in the money:</u>", text_alignment= "center", unsafe_allow_html=True)
+    st.markdown("follows from $d_2$", text_alignment= "center")
+
+    st.space(size="small")
+    st.markdown("<u>probability how far option ends in the money:</u>", text_alignment= "center", unsafe_allow_html=True)
+    st.markdown("Is indicated by the addition of the standard deviation instead of the subtraction in $d_2$", text_alignment= "center")
+    st.latex(r"d_1 = \left( r+\frac{ \sigma^2 }{ 2 } \right)")
+    st.latex(r"d_2 = \left( r-\frac{ \sigma^2 }{ 2 } \right)")
+    st.markdown("The reason behind this is, that the option holder benefits from positive price movements, if the strike price gets reached. Which increases the value of the option and consequently the option-price", text_alignment= "center")
+
+with col1:
+    st.space(size="small")
     st.latex(r"d_2 = \frac{ ln(\frac{ A }{ K })+(r-\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }")
     st.markdown("the probability that the strike price will be reached at expiration", text_alignment= "center")
 
@@ -31,12 +47,28 @@ with col2:
 
 
     st.space(size="small")
-    st.markdown("the requiered return:", text_alignment= "center")
+    st.markdown("<u>the requiered return:</u>", text_alignment= "center", unsafe_allow_html=True)
     st.latex(r"A*e^{ rd } = K")
     st.latex(r"e^{ rd } = \frac{ K }{ A }")
     st.latex(r"rd = ln\left( \frac{ K }{ A } \right)")
 
     st.space(size="small")
-    st.markdown("the expected return:", text_alignment= "center")
+    st.markdown("<u>the expected return:</u>", text_alignment= "center", unsafe_allow_html=True)
     st.latex(r"\left( r-\frac{ \sigma^2 }{ 2 } \right)\left( t \right)")
-    st.markdown("Note:  \nInterest rates drive growth; fluctuations reduce growth; time amplifies both", text_alignment= "center") 
+    st.markdown("Note:  \nInterest rates drive growth; fluctuations reduce growth; time amplifies both", text_alignment= "center")
+
+    st.space(size="small")
+    st.markdown("<u>the standard deviation:</u>", text_alignment= "center", unsafe_allow_html=True)
+    st.latex(r"\sigma\sqrt{ t }")
+    st.markdown("counts the standard deviations over time", text_alignment= "center")
+    st.caption("Since the Gaussian bell curve is defined with a standard deviation of one, this division allows the formula to be used for all stocks", text_alignment= "center")
+    
+    st.space(size="small")
+    st.markdown("Put together, the formula looks like this:", text_alignment= "center")
+    st.latex(r"\frac{ ln(\frac{ K }{ A })-(r-\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }")
+    st.markdown("But this doesn't equal with the formula at the top", text_alignment= "center")
+    st.markdown("Since the normal distribution is defined from negative infinity and the normal distribution has the property of being symmetrical, a sign change can be made to calculate the standard deviation to the right of the curve", text_alignment="center")
+    st.latex(r"-\left(\frac{ ln(\frac{ K }{ A })-(r-\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }\right)")
+    st.latex(r"= -\frac{ ln(K)-ln(A)-(r-\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }")
+    st.latex(r"= \frac{ -ln(K)+ln(A)+(r-\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }")
+    st.latex(r"= \frac{ ln(\frac{ A }{ K })+(r-\frac{ \sigma^{ 2 } }{ 2 })(t) }{ \sigma\sqrt{ t } }")
