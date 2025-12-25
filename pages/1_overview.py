98a0d89eca35060aac104a2bd784c85591ccecbd
Yannick Getto <yannickgetto47@gmail.com>
2025-12-25 20:23:12 +0100

    simulated asset graph. Contained with asset price, strike price and volatility. Next step: understanding the normal distribution and the GBM (Geometric Brownian Motion) so I can implement a graph to improve the visual understanding.

--- pages/1_overview.py
+++ pages/1_overview.py
@@ -3,41 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from streamlit.elements.media import YOUTUBE_RE
 
-asset_price = 100
-sigma = 0.2
-x_1 = np.arange(0.0, 20, 0.3)
-x_2 = np.arange(20, 25, 0.3)
 
-
-last_value = 10
-y_1 = np.array([])
-for val in x_1:
-    if (last_value >= 5):
-        rand = np.random.randint(last_value - 1, last_value + 3)
-    else:
-        rand=np.random.randint(0, last_value + 3)
-
-    last_value = rand
-    y_1 = np.append(y_1, rand)
-
-fig1, ax = plt.subplots()
-
-ax.plot(x_1, y_1,)
-
-
-#y_2 = last_value + np.sin(sigma * np.pi*5 * x_2)
-last_value2 = last_value
-y_2 = np.array([])
-y_3 = np.array([])
-for val in x_2:
-    y_2 = np.append(y_2, last_value)
-    y_3 = np.append(y_3, last_value2)
-    last_value = last_value + sigma
-    last_value2 = last_value2 - sigma
-
-ax.plot(x_2, y_2, color="#f5ad42")
-ax.plot(x_2, y_3, color="#f5ad42")
-
-st.pyplot(fig1)
 
 
--- pages/2_test.py
+++ pages/2_test.py
@@ -1,3 +1,37 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title("Test")
+
+P0 = 100
+P_target = 110
+strike_price = 120
+n_days = 100
+# volatility per day
+sigma = 0.02
+t = 0.4
+
+# Calculate the required total log return -> also used in Black-Scholes
+total_log_return = np.log(P_target / P0)
+
+log_returns = np.random.normal(0, sigma, n_days)
+
+sum_log_returns = np.sum(log_returns)
+
+log_returns_corrected = log_returns * (total_log_return / sum_log_returns)
+
+prices = P0 * np.exp(np.cumsum(log_returns_corrected))
+
+fig, ax = plt.subplots()
+ax.plot(prices)
+ax.set_title("Simulierter Aktienkurs")
+ax.set_xlabel("Zeit")
+ax.set_ylabel("Preis")
+
+
+
+
+# strike price line
+ax.axhline(y=strike_price, color='r', linestyle='--')
+
+st.pyplot(fig)
